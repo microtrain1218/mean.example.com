104a2956d1caa0c8ef19ffeecd075f5b639ca657
Jason Snider <root@jasonsnider.com>
2019-01-17 14:58:34 -0600

    Implement a users endpoit

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 var mongoose = require('mongoose');
 
+var apiUsersRouter = require('./routes/api/users');
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
@@ -25,6 +26,7 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
+app.use('/api/users', apiUsersRouter);
 app.use('/users', usersRouter);
 
 // catch 404 and forward to error handler
